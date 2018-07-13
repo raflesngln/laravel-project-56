e70146ae89df930c44172d112e67c9d6b528f084
Rafles N <raflesngln@gmail.com>
2018-07-13 16:42:18 +0700

    revisi api

--- routes/api.php
+++ routes/api.php
@@ -6,35 +6,31 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-
 /* Companies API 
 access postman like with this:
+For adding data Method POST                 :  http://localhost:8000/api/v1/companies
 for all data  methode GET                   :  http://localhost:8000/api/v1/companies
 for detail with id  methode GET             :  http://localhost:8000/api/v1/companies/2
 for update methode PUT                      :  http://localhost:8000/api/v1/companies/4
 for delete use methode DELETE               :  http://localhost:8000/api/v1/companies/5
 */
 Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
-    Route::resource('companies', 'CompaniesController', ['except' => ['create', 'edit']]);
+    Route::resource('companies', 'CompaniesController', ['except' => ['create', 'edit']]); //kecuali fungsi create dan edit form
 });
-
 
 Route::post('upload_file', function(Request $request){
     $file = $request->file('photo')->store('uploads');
     return dd($file);
     // var_dump(Input::file('file'));
-
 });
 
 
@@ -43,6 +39,7 @@ Route::post('upload_file', function(Request $request){
 Route::get('items', function(){
     return Product::paginate('2');
 });
+
 Route::get('items/{item}', function($id){
     return Product::find($id);
 });
@@ -57,8 +54,8 @@ Route::delete('items/{item}', function(Product $item){
 });
 
 Route::put('items/{id}', function(Product $request, $id){
-
     $product = Product::find($id);
     $product->update($request->all());
     return $product;
 });
+
